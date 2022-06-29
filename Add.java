9586ee946d9bdb9fe9f70e727b3473d8cce0a88d
Arun kumar <107771281+Arunkr44@users.noreply.github.com>
2022-06-29 12:06:40 +0530

    add

---
+++ Add.java
@@ -0,0 +1,8 @@
+public class Add {
+    public static void main (String args[]){
+    int a =29;
+    int b =30;
+    int sum = a + b;
+    System.out.println("The sum of a and b is : "  + sum);
+}
+}
